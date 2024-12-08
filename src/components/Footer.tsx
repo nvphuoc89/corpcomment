5bf70f8d3798a040a695fcc328860327e0a5135a
Phuoc Nguyen <nguyenvinhphuoc@outlook.com>
2024-12-08 14:42:16 +0700

    add initial components: Logo, Pattern, PageHeading, FeedbackForm, FeedbackList, Container, HashtagList, Header, Footer; update App structure and remove unused assets

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          &copy; Copyright by{" "}
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/phuoc-nguyenvinh/"
+          >
+            Phuoc
+          </a>
+          . Intended for learning or your portfolio.
+        </p>
+        <p>
+          <span className="u-bold u-italic">Not allowed</span> to use as your
+          own teaching material.
+        </p>
+      </small>
+    </footer>
+  );
+}
